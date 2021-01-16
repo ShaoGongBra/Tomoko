52097d81ce8887cec01f3cab65811de41d07fe21
ShaoGongBra <31879769+ShaoGongBra@users.noreply.github.com>
2021-01-16 11:12:06 +0800

    1

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log(1213)
+const Koa = require('koa')
+const compose = require('koa-compose')
+const app = new Koa()
+
+// 日志函数
+function logger(format = ':method :time :url') {
+  return async (ctx, next) => {
+    const startTime = Date.now()
+    await next()
+    const endTime = Date.now()
+    const str = format
+      .replace(':method', ctx.method)
+      .replace(':time', endTime - startTime + 'ms')
+      .replace(':url', ctx.url)
+    console.log(str)
+  }
+}
+
+// 耗时函数
+function timeout(time = 100) {
+  function asyncTimeout() {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(), time)
+    })
+  }
+  return async (ctx, next) => {
+    await asyncTimeout()
+    await next()
+  }
+}
+
+async function body(ctx, next) {
+  ctx.body = '请求成功'
+}
+
+app.use(compose([logger(), timeout(), body]))
+
+app.listen(3000)
+console.log('http://127.0.0.1:3000')
